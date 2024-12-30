b7989d42fe86f77d4612bfbdb00f275b40afb058
VipeR-69 <computershubham7@gmail.com>
2024-12-30 23:53:23 +0530

    added render host

--- myapp/settings.py
+++ myapp/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", cast=bool)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.pythonanywhere.com', '.railway.app']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.pythonanywhere.com', '.railway.app','.onrender.com']
 
 
 # Application definition
