342aebcef286b6dc1d1ce438c28f206cd23c858b
VipeR-69 <computershubham7@gmail.com>
2024-12-31 00:57:08 +0530

    changed the phone number datatype to CharField for client model

--- index/models.py
+++ index/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Client(models.Model):
     firstname = models.CharField(max_length=20)
     lastname = models.CharField(max_length=20)
-    phone = models.IntegerField()
+    phone = models.CharField(max_length=10)
     username = models.CharField(max_length=30)
     email = models.CharField(max_length=50)
     password = models.CharField(max_length=100)
